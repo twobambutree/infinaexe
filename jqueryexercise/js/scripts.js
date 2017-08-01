f1b0d4e8b8c9c1adb53aa71a34c480d938223791
twobambutree <patiphan@gmail.com>
2017-08-01 19:44:24 -0400

    2nd commit

--- jqueryexercise/js/scripts.js
+++ jqueryexercise/js/scripts.js
@@ -1,13 +1,33 @@
-//after click hiding the 1st slide
-$("button a").click(function () {
-    $("#first").hide();
-});
-// hide all divs is second div
-$("div#second > div").hide();
-// Selector
-var divs = $("#one, #two, #three, #four");
-// Show chosen div, and hide all others
-$("button a").click(function () {
-    $(divs).hide();
-    $("#" + $(this).attr("class")).show();
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+	$(".next").click(function() {
+  		var from = $(this).parent().parent().parent().attr("id");
+  		from = '#' + from;
+  		$( from ).addClass( "hide" );
+  		var to = from.substr(from.length -1);
+  		to = parseInt(to) + 1;
+  		to = '#page-' + to;
+  		$( to ).removeClass( "hide" );
+	});
+
+  $(".previous").click(function() {
+      var from = $(this).parent().parent().parent().attr("id");
+      from = '#' + from;
+      $( from ).addClass( "hide" );
+      var to = from.substr(from.length -1);
+      to = parseInt(to) - 1;
+      to = '#page-' + to;
+      $( to ).removeClass( "hide" );
+
+  });
+
+  $(".start").click(function() {
+      var from = $(this).parent().parent().parent().attr("id");
+      from = '#' + from;
+      $( from ).addClass( "hide" );
+      to = '#page-1';
+      $( to ).removeClass( "hide" );
+
+  });
 });
--- phpexercise/index.php
+++ phpexercise/index.php
@@ -1,6 +1,27 @@
-<?php 
+<?php    
+$str = file_get_contents('data.json');
+$json = json_decode($str, true);
+$label = [];
+$op01 = "";
+$op02 = "";
+$op03 = "";
 
+foreach ($json as $key => $value) {
+    array_push($label, $key);
+    foreach ($value as $key02 => $val02) {
+        //var_dump($val02['value']);
+        //var_dump($val02['text']);
+        if ($key == "Airspace") {
+            $op01 .= "<option value=".$val02['value'].">".$val02['text']."</option>";  
+        } elseif ($key == "Control Surface") {
+            $op02 .= "<option value=".$val02['value'].">".$val02['text']."</option>"; 
+        } elseif ($key == "Deviation Types"){
+            $op03 .= "<option value=".$val02['value'].">".$val02['text']."</option>"; 
+        }
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,34 +30,22 @@
 <body>
 
 <h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
+<p>This is a paragra<?php echo "hi"; ?></p>
 
 <form action="select.php" method='post'>
+<label><?php echo $label[0];?>:</label>
+<select name = "<?php echo str_replace(' ', '_', $label[0]);?>">
+    <?php echo $op01; ?>
+</select>
+<label><?php echo $label[1]?>:</label>
+<select name = "<?php echo str_replace(' ', '_', $label[1]);?>">
+    <?php echo $op02; ?>
+</select>
+<label><?php echo $label[2]?>:</label>
+<select name = "<?php echo str_replace(' ', '_', $label[2]);?>">
+    <?php echo $op03; ?>
+</select>
 
-<?php    
-$str = file_get_contents('data.json');
-$json = json_decode($str, true);
-
-//  Scan through outer loop
-foreach ($json as $innerArray => $name) {
-    echo $innerArray;
-    $i = str_replace(" ", "_", $innerArray);
-    echo "<select name='$i'>";
-    //  Check type
-    if (is_array($name)){
-    //  Scan through inner loop
-        foreach ($name as $value) {
-            if(is_array( $value)){
-                foreach ($value as $key=>$item){} 
-                //for some reason the array is nested farther.
-            }
-            echo "<option>$item</option>";
-        }
-    }
-    echo "</select>      \n\n\n\n\n";
-}
-?>
 <input type="submit" name="submit" value="submt"/>
 </form>
 
